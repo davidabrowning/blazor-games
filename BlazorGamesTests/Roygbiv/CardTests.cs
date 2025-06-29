fc3b6df0e691b07b28cb2591a9d9b1c4c56ab996
David Browning <david.a.browning@gmail.com>
2025-06-29 19:15:56 +0200

    Test for card inequality.

--- BlazorGamesTests/Roygbiv/CardTests.cs
+++ BlazorGamesTests/Roygbiv/CardTests.cs
@@ -12,5 +12,13 @@ namespace BlazorGamesTests.Roygbiv
             Card c2 = new Card() { Value = 7 };
             Assert.Equal(c1, c2);
         }
+
+        [Fact]
+        public void Unequal_Cards_Are_Not_Equal()
+        {
+            Card c1 = new Card() { Value = 7 };
+            Card c2 = new Card() { Value = 8 };
+            Assert.NotEqual(c1, c2);
+        }
     }
 }
