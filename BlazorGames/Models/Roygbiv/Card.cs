aebfe778428c3dc763ccb8b9d5144f738aeedaa4
David Browning <david.a.browning@gmail.com>
2025-06-29 19:08:32 +0200

    Add first unit tests.

--- BlazorGames/Models/Roygbiv/Card.cs
+++ BlazorGames/Models/Roygbiv/Card.cs
@@ -3,5 +3,17 @@
     public class Card
     {
         public int Value { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is Card))
+                return false;
+            Card cardObj = (Card)obj;
+            if (cardObj.Value != Value)
+                return false;
+            return true;
+        }
     }
 }
---
+++ BlazorGamesTests/Roygbiv/CardTests.cs
@@ -0,0 +1,16 @@
+﻿using BlazorGames.Models.Roygbiv;
+using Xunit;
+
+namespace BlazorGamesTests.Roygbiv
+{
+    public class CardTests
+    {
+        [Fact]
+        public void Equal_Cards_Are_Equal()
+        {
+            Card c1 = new Card() { Value = 7 };
+            Card c2 = new Card() { Value = 7 };
+            Assert.Equal(c1, c2);
+        }
+    }
+}
